a32f8f758f474b47f7dc118ca367e97753b09ddd
boostedgagi <dragan.02jelic@gmail.com>
2022-12-02 11:05:42 +0100

    Added profiler for testing performances of the code. Configuration files updated by themselves.

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
