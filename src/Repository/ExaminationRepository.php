b34db97010c6350389c3d063b7f8d0232fca389c
boostedgagi <dragan.02jelic@gmail.com>
2022-12-02 13:17:18 +0100

    Examination entity, migrated, workbench connect to mysql server port.

---
+++ src/Repository/ExaminationRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Examination;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Examination>
+ *
+ * @method Examination|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Examination|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Examination[]    findAll()
+ * @method Examination[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ExaminationRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Examination::class);
+    }
+
+    public function save(Examination $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Examination $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return Examination[] Returns an array of Examination objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('e')
+//            ->andWhere('e.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('e.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Examination
+//    {
+//        return $this->createQueryBuilder('e')
+//            ->andWhere('e.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
