ee04cf07729ab33950a71a8203fb54baec0c3517
boostedgagi <dragan.02jelic@gmail.com>
2022-12-04 12:38:41 +0100

    Pet form type made.

---
+++ src/Form/PetType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Pet;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PetType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('dateOfBirth')
+            ->add('animal')
+            ->add('breed')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('image')
+            ->add('owner')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Pet::class,
+        ]);
+    }
+}
