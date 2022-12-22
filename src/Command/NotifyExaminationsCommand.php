ba3db5019b0ab3c7ae0567270adb99816f8ad18c
boostedgagi <dragan.02jelic@gmail.com>
2022-12-22 13:38:06 +0100

    NotifyExaminations command is made.

---
+++ src/Command/NotifyExaminationsCommand.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Command;
+
+use App\Repository\HealthRecordRepository;
+use App\Service\EmailRepository;
+use PhpParser\Node\Stmt\Return_;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Notifier\NotifierInterface;
+
+#[AsCommand(
+    name: 'notify-examinations',
+    description: 'Notify all pet owners whose pets have an scheduled examination in the next 7 days',
+    aliases: ['notify:examinations']
+)]
+class NotifyExaminationsCommand extends Command
+{
+    private MailerInterface $mailer;
+    private NotifierInterface $notifier;
+    private HealthRecordRepository $healthRecRepo;
+
+    /**
+     * @param MailerInterface $mailer
+     * @param NotifierInterface $notifier
+     * @param HealthRecordRepository $healthRecRepo
+     */
+    public function __construct(MailerInterface $mailer, NotifierInterface $notifier, HealthRecordRepository $healthRecRepo)
+    {
+        $this->mailer = $mailer;
+        $this->notifier = $notifier;
+        $this->healthRecRepo = $healthRecRepo;
+
+        parent::__construct();
+
+    }
+
+
+    protected function configure(): void
+    {
+//        $this
+//            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+//            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+//        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln([
+            'Okay let\'s notify pet owners!',
+            '============',
+            '',
+        ]);
+//        $arg1 = $input->getArgument('arg1');
+//
+//        if ($arg1) {
+//            $io->note(sprintf('You passed an argument: %s', $arg1));
+//        }
+//
+//        if ($input->getOption('option1')) {
+//            // ...
+//        }
+
+        $examinationsToRemind = $this->healthRecRepo->getExaminationsInNextSevenDays();
+
+        if(count($examinationsToRemind)===0){
+            $output->writeln([
+                'Great! But...',
+                '',
+                'All users are already notified!'
+            ]);
+            return Command::SUCCESS;
+        }
+        foreach ($examinationsToRemind as $examination) {
+            try {
+                $email = new EmailRepository($this->mailer);
+                //$examination is the HealthRecordType
+                $email->notifyUserAboutPetHaircut($this->notifier, $examination);
+
+            } catch (\Exception $exception) {
+                return Command::FAILURE;
+            }
+        }
+
+        $output->writeln([
+            'Great!',
+            '',
+            'Users are notified!'
+        ]);
+        return Command::SUCCESS;
+    }
+}
