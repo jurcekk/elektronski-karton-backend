195b15b583921f8cc9ba8c6f6ddeff9e48c45488
boostedgagi <dragan.02jelic@gmail.com>
2023-04-17 12:33:04 +0200

    Helper class made.

---
+++ src/Helper.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App;
+
+class Helper
+{
+    public const ONE_HOUR_IN_SECONDS = 3600;
+}
--- src/Service/TokenRepository.php
+++ src/Service/TokenRepository.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 
+use App\Helper;
 use DateTime;
 use Doctrine\ORM\Mapping\Entity;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,7 +16,7 @@ class TokenRepository
         $hashedToken = md5(uniqid('', true) . mt_rand(10, 100));
 
         //duration of token is 30minutes
-        $expires = strtotime(date('Y-m-d h:i:s')) + (60 * 30);
+        $expires = strtotime(date('Y-m-d h:i:s')) + (Helper::ONE_HOUR_IN_SECONDS / 2);
 
         return (object)[
             'tokenItself' => $hashedToken,
