0921c8cbf448b5a91105407b90191d082d8fec35
boostedgagi <dragan.02jelic@gmail.com>
2023-04-29 23:03:57 +0200

    Added more strict check on petFormType class.

--- src/Form/PetType.php
+++ src/Form/PetType.php
@@ -7,6 +7,7 @@ use App\Entity\User;
 use DateTimeImmutable;
 use Doctrine\DBAL\Types\DateTimeImmutableType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,10 +18,14 @@ class PetType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('dateOfBirth',null,[
-                'widget'=>'single_text'
+            ->add('dateOfBirth', null, [
+                'widget' => 'single_text'
             ])
-            ->add('animal')
+            ->add('animal', ChoiceType::class, [
+                'choices' => [
+                    'Dog' => 'Dog',
+                    'Cat' => 'Cat'
+                ]])
             ->add('breed')
             ->add('owner');
     }
